fd1da06296a538c69d7c1dd3dc441bd32ef54a3f
yotsuyanagi <yotsuyanagi@torico-tokyo.com>
2017-04-19 10:51:13 +0900

    add jjfile

---
+++ jjfile.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+menu = [
+    ('cancel',
+     ""),
+    ('flake8',
+     "flake8 ."),
+    ('register pypi',
+     "./setup.py register"),
+    ('upload pypi',
+     "./setup.py sdist upload"),
+]
